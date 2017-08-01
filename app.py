b1239cd876144f5a29b1e2b1560fd016bf3c2ce2
georgea18-meet <abudauodg@gmail.com>
2017-08-01 18:35:46 +0300

    Mainpage and stuff

--- app.py
+++ app.py
@@ -20,8 +20,9 @@ def hello_world():
 
 @app.route('/countries/<int:continent>')
 def flags_menu(continent):
-	countries = session.query(Country).filter_by(continent=continent).all()
-	return render_template('flagslist.html',countries = countries,continent=continent)
+	countries = session.query(Country).filter_by(continet=continent).all()
+	c = session.query(Continent).filter_by(id=continent).first()
+	return render_template('flagslist.html',countries = countries,continent=c)
 
 @app.route('/add/<int:country>', methods=['GET','POST'])
 def adding_form(country):
@@ -56,7 +57,7 @@ def addinfo(country):
 	if request.method == 'GET':
 		return render_template('addinfo.html',country=c,continents=continents)
 	else:
-		c.continent = request.form.get(continent)
-		c.flag = request.form.get(flag)
+		c.continet = request.form.get('continent')
+		c.flag = request.form.get('flag')
 		session.commit()
 		return redirect(url_for('addinfo',country=country))
